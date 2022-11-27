49d1a547484a521530f6dac7547c3f2f592959da
EduardoAyora <eduardoaayora64@gmail.com>
2022-11-27 17:15:38 -0500

    Mostrar seleccion

--- principal.cpp
+++ principal.cpp
@@ -1,45 +1,157 @@
 #include "header.h"
 
-Mat frame;
-Mat frameRecortado;
+// Mat frame;
+// Mat frameRecortado;
 
-void showFrames()
+// void showFrames()
+// {
+//   Mat ventana(Size(frame.cols * 3, frame.rows), frame.type());
+
+//   putText(frame, "Original", Point(50, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+
+//   frame.copyTo(ventana(Range(0, frame.rows), Range(0, frame.cols)));
+//   frameRecortado.copyTo(ventana(Range(0, frame.rows), Range(frame.cols, frame.cols * 2)));
+//   imshow("Video", ventana);
+// }
+
+// void eventoTrack(int porcentaje, void *data)
+// {
+// }
+
+// int main(int argc, char *argv[])
+// {
+//   namedWindow("Video", WINDOW_AUTOSIZE);
+
+//   // createTrackbar("Ruido de sal", "Video", &porcentajeRuido, 100, eventoTrackRuidoSal, NULL);
+
+//   VideoCapture video(0);
+//   if (!video.isOpened())
+//     return 0;
+
+//   while (true)
+//   {
+//     video >> frame;
+//     resize(frame, frame, Size(), 0.70, 0.70);
+//     frameRecortado = frame.clone();
+
+//     showFrames();
+
+//     if (waitKey(23) == 27)
+//       break;
+//   }
+
+//   destroyAllWindows();
+//   return 0;
+// }
+
+bool esPuntoVacio(Point punto)
 {
-  Mat ventana(Size(frame.cols * 3, frame.rows), frame.type());
-
-  putText(frame, "Original", Point(50, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
-
-  frame.copyTo(ventana(Range(0, frame.rows), Range(0, frame.cols)));
-  frameRecortado.copyTo(ventana(Range(0, frame.rows), Range(frame.cols, frame.cols * 2)));
-  imshow("Video", ventana);
+  if (punto.x == 0 && punto.y == 0)
+    return true;
+  return false;
 }
 
-void eventoTrack(int porcentaje, void *data)
+bool sonClicsPermitidos = true;
+Mat img;
+Mat areaSeleccionada;
+Point primerPunto;
+Point puntoAnterior;
+
+Mat convertirEnImagenNegra(Mat imagen, bool esImagenConColores)
 {
-}
-
-int main(int argc, char *argv[])
-{
-  namedWindow("Video", WINDOW_AUTOSIZE);
-
-  // createTrackbar("Ruido de sal", "Video", &porcentajeRuido, 100, eventoTrackRuidoSal, NULL);
-
-  VideoCapture video(0);
-  if (!video.isOpened())
-    return 0;
-
-  while (true)
+  Mat imagenTmp = imagen.clone();
+  for (int i = 0; i < imagenTmp.rows; i++)
   {
-    video >> frame;
-    resize(frame, frame, Size(), 0.70, 0.70);
-    frameRecortado = frame.clone();
+    for (int j = 0; j < imagenTmp.cols; j++)
+    {
+      if (esImagenConColores)
+      {
+        Vec3b color = imagenTmp.at<Vec3b>(i, j);
+        color[0] = 0;
+        color[1] = 0;
+        color[2] = 0;
+        imagenTmp.at<Vec3b>(i, j) = color;
+      }
+      else
+      {
+        imagenTmp.at<uchar>(i, j) = 0;
+      }
+    }
+  }
+  return imagenTmp;
+}
 
-    showFrames();
+Mat obtenerImagenRecortada()
+{
+  Mat frameRecortado = convertirEnImagenNegra(img, true);
+  for (int i = 0; i < areaSeleccionada.rows; i++)
+  {
+    bool esDentroDeFigura = false;
+    for (int j = 0; j < areaSeleccionada.cols; j++)
+    {
+      if ((int)areaSeleccionada.at<uchar>(i, j) > 0)
+        esDentroDeFigura = !esDentroDeFigura;
+      if (esDentroDeFigura)
+      {
+        Vec3b color = img.at<Vec3b>(i, j);
+        frameRecortado.at<Vec3b>(i, j) = color;
+      }
+    }
+  }
+  return frameRecortado;
+}
 
-    if (waitKey(23) == 27)
-      break;
+int thickness = 1;
+
+void mouse_call(int event, int x, int y, int, void *)
+{
+  if (!sonClicsPermitidos)
+    return;
+  Point puntoActual = Point(x, y);
+  if (event == EVENT_LBUTTONDOWN)
+  {
+    if (puntoAnterior.x != 0 && puntoAnterior.y != 0)
+    {
+      line(img, puntoActual, puntoAnterior, Scalar(0, 0, 255), thickness, LINE_AA);
+      line(areaSeleccionada, puntoActual, puntoAnterior, 255, thickness, LINE_AA);
+    }
+    else
+    {
+      primerPunto = puntoActual;
+    }
+    circle(img, puntoActual, 0, Scalar(0, 0, 255), 1, 8, 0);
+    puntoAnterior = puntoActual;
   }
 
-  destroyAllWindows();
+  if (event == EVENT_RBUTTONDOWN)
+  {
+    line(img, primerPunto, puntoAnterior, Scalar(0, 0, 255), thickness, LINE_AA);
+    line(areaSeleccionada, primerPunto, puntoAnterior, 255, thickness, LINE_AA);
+    namedWindow("Recortada");
+    imshow("Recortada", obtenerImagenRecortada());
+    sonClicsPermitidos = false;
+  }
+  imshow("Original", img);
+  imshow("Area seleccionada", areaSeleccionada);
+}
+
+int main()
+{
+  img = imread("../detector_movimiento/mario.webp");
+  cvtColor(img, areaSeleccionada, COLOR_BGR2GRAY);
+
+  // convertir area seleccionada en imagen negra
+  areaSeleccionada = convertirEnImagenNegra(areaSeleccionada, false);
+
+  namedWindow("Original");
+  namedWindow("Area seleccionada");
+  imshow("Original", img);
+  imshow("Area seleccionada", areaSeleccionada);
+
+  setMouseCallback("Original", mouse_call);
+
+  while (char(waitKey(1) != 'q'))
+  {
+  }
   return 0;
 }
